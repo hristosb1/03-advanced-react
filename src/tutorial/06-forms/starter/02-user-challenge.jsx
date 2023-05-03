59ffced8f1e8a977cf6bc0283d8e28877e06c9b1
hristosb1 <hristo.s.burgazliev@gmail.com>
2023-05-03 19:20:35 +0300

    finished with forms

--- src/tutorial/06-forms/starter/02-user-challenge.jsx
+++ src/tutorial/06-forms/starter/02-user-challenge.jsx
@@ -1,20 +1,64 @@
+import { useState } from "react";
+import { data } from "../../../data";
+
 const UserChallenge = () => {
+  const [name, setName] = useState("");
+  const [users, setUsers] = useState(data);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name) return;
+    let id = users.length + 1;
+    console.log(id);
+    const newUser = { id: id, name: name };
+    const updated = [...users, newUser];
+    setUsers(updated);
+    setName("");
+  };
+  const removeUser = (id) => {
+    const updated = users.filter((person) => person.id != id);
+    setUsers(updated);
+  };
+
   return (
     <div>
-      <form className='form'>
+      <form className="form" onSubmit={handleSubmit}>
         <h4>Add User</h4>
-        <div className='form-row'>
-          <label htmlFor='name' className='form-label'>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
             name
           </label>
-          <input type='text' className='form-input' id='name' />
+          <input
+            type="text"
+            className="form-input"
+            id="name"
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
         </div>
 
-        <button type='submit' className='btn btn-block'>
+        <button type="submit" className="btn btn-block">
           submit
         </button>
       </form>
-      {/* render users below */}
+      <h2>users:</h2>
+      {users.map((user) => {
+        return (
+          <div key={user.id}>
+            <h4>{user.name}</h4>
+            <button
+              className="btn"
+              onClick={() => {
+                removeUser(user.id);
+              }}
+            >
+              remove
+            </button>
+          </div>
+        );
+      })}
     </div>
   );
 };
